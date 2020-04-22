a363db919ae71b403758f8674c126c4663aeb793
Albert Cervera i Areny <albert@nan-tic.com>
2020-04-22 02:12:20 +0200

    Update to 5.4

--- bank.py
+++ bank.py
@@ -3,10 +3,7 @@
 from trytond.pool import PoolMeta
 from trytond.model import fields
 
-__all__ = ['BankAccount']
 
-
-class BankAccount:
-    __metaclass__ = PoolMeta
+class BankAccount(metaclass=PoolMeta):
     __name__ = 'bank.account'
     account = fields.Many2One('account.account', 'Account')
